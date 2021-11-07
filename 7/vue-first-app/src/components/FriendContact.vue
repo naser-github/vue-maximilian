e240d582e2fb9f3ed3a4c547b42e1340c86adcaa
naser0 <abu.naser@alibaba-inc.com>
2021-11-07 18:11:35 +0600

    till 7

---
+++ 7/vue-first-app/src/components/FriendContact.vue
@@ -0,0 +1,48 @@
+<template>
+    <section>
+        <ul>
+            <li>
+                <strong>{{friend.name}}</strong>
+            </li>
+
+            <button @click="toggleDetails">
+                {{showDetails? 'Hide': 'Show'}} Details
+            </button>
+
+            <ul v-if="showDetails">
+                <li>
+                    <strong> Phone: </strong> 
+                    {{friend.phone}}
+                </li>
+                <li>
+                    <strong> Email: </strong> 
+                    {{friend.email}}
+                </li>
+            </ul>
+        </ul>
+    </section>
+</template>
+
+<script>
+ 
+export default {
+    data(){
+        return {
+            showDetails: false,
+            friend: {
+                id:'latif',
+                name:'Latif Kabir',
+                phone:'0123456789',
+                email:'latif@vue.com',
+            },
+                
+        };
+    },
+
+    methods: {
+        toggleDetails(){
+            this.showDetails = !this.showDetails;
+        }
+    }
+}
+</script>
