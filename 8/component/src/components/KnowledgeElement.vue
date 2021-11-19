679a1fe9fe5309366ab2ad9a02ee5bf2b6cef3a8
tayson13 <ab.naser01@gmail.com>
2021-11-20 04:31:13 +0600

    8-13

---
+++ 8/component/src/components/KnowledgeElement.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h3>{{ topicName }}</h3>
+
+    <p>{{ description }}</p>
+
+    <button @click="$emit('select-topic', id)">Learn More</button>
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['id', 'topicName', 'description'],
+
+  emits: ['select-topic'],
+};
+</script>
